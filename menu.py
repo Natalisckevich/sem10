0eefebe40cf783c8854ebf1da246a45b175d38c8
Natasha <lickevich.natasha@rambler.ru>
2022-10-09 21:29:02 +0300

    menu.py

---
+++ menu.py
@@ -0,0 +1,7 @@
+import Func.sum_mod as sum
+import Func.sub_mod as sub
+import Func.mult_mod as mult
+import Func.div_mod as div
+
+dict_arith = {'+': sum, '-': sub, '*': mult, '/': div}
+dict_log = {'+':'сложение','-':'вычитание','*':'умножение','/':'деление'}
